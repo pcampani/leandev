8229fe21a85cb40e04650bc251ef9ca15fdf144e
pcampani <toycampani@gmail.com>
2020-03-22 11:37:31 +0800

    Set top footer margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 
 const Footer = styled.div`
-  margin-top: 3rem;
+  margin-top: 6rem;
   color: #fff;
   display:grid;
   place-content: center;
