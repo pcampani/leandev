bd0189ac753871e49a259cc00ba082f351b21917
pcampani <toycampani@gmail.com>
2020-03-23 09:01:15 +0800

    Optimize bg image

--- src/components/Stylesheets.js
+++ src/components/Stylesheets.js
@@ -19,13 +19,3 @@ export const Wrapper = styled.div`
     justify-content: center;
   }
 `
-export const pageTransition = {
-  in: {
-    opacity:1,
-    x: 0
-  },
-  out: {
-    opacity:0,
-    x: '100vw'
-  }
-}
